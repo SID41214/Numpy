9239dacdf9fac07c711c8e986dfc34fcd76c9754
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-08-01 12:04:44 +0530

    updated

---
+++ wr.py
@@ -0,0 +1,12 @@
+import statistics
+
+data = [1, 2, 3, 4, 5]
+# data = [1, 2,2,2, 3, 4, 5]
+
+mode = statistics.mode(data)
+print(mode)
+print("-------------------------------")
+import sys
+print(sys.version)
+print("-------------------------------")
+
